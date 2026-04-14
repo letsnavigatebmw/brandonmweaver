323dc25ae3a028dc63f75c7a26fd92e7c19fa780
Brandon Weaver <brandon@navigatenow.com>
2026-04-13 18:16:43 -0700

    Apply CSS filter to Sonder logo to convert green to black

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -142,7 +142,8 @@ Prior roles include Chief Commercial Officer at Lacure, executive leadership at 
                             <img 
                               src={experiences[0].logo} 
                               alt={experiences[0].name}
-                              className="h-8 md:h-10 w-auto bg-black px-3 py-2 rounded"
+                              className="h-8 md:h-10 w-auto px-3 py-2 rounded bg-black"
+                              style={experiences[0].name === 'Sonder Hospitality' ? { filter: 'brightness(0.2) saturate(0) invert(1)' } : {}}
                             />
                           </>
                         )}
@@ -280,7 +281,8 @@ Prior roles include Chief Commercial Officer at Lacure, executive leadership at 
                           <img 
                             src={exp.logo} 
                             alt={exp.name}
-                            className="h-8 md:h-10 w-auto bg-black px-3 py-2 rounded"
+                            className="h-8 md:h-10 w-auto px-3 py-2 rounded bg-black"
+                            style={exp.name === 'Sonder Hospitality' ? { filter: 'brightness(0.2) saturate(0) invert(1)' } : {}}
                           />
                         </>
                       )}
